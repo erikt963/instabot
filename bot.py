59b96703eed1e117afe0e5a3d7b07c809786893f
abd <abdallawy3@gmail.com>
2023-01-05 02:10:03 +0200

    'bot'

--- bot.py
+++ bot.py
@@ -1,1 +1,7 @@
-from instabo: 
+from instabot import Bot 
+new_bot = Bot()
+new_bot.login(username="erikbo848", password="erik.848.981")
+
+# message = "Hi heba"
+# new_bot.send_message(message, "heba_astef")
+new_bot.follow("heba_astef")
