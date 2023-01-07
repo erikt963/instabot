41379dc03a0ba807aa099208ed2fc336d8e8e922
abd <abdallawy3@gmail.com>
2023-01-07 12:57:41 +0200

    'bot'

--- bot.py
+++ bot.py
@@ -4,10 +4,13 @@ from instabot import Bot
 # cookie_del = glob.glob("config/*cookie.json")
 # os.remove(cookie_del[0])
 bot = Bot()
-bot.login(username="erikbo848",
-          password="erik.848")
+bot.login(username="eriktbo981",
+          password="new.bot.848")
  
 # Follow
  
-# To follow single person.
-bot.follow("geeks_for_geeks")
+message = "hboooooosh"
+bot.send_message(message, "heba_astef")
+followers = bot.get_user_followers("heba_astef")
+print("her followers")
+print(len(followers))
